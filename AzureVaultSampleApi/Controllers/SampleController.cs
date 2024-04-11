621355d83e6cd2c0cea0e884532f1087f5cc3282
lenabsJyn2474747 <jepozdemir@gmail.com>
2024-04-11 15:04:12 +0300

    initial

---
+++ AzureVaultSampleApi/Controllers/SampleController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace AzureVaultSampleApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SampleController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public SampleController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult GetSecret()
+        {
+            var secretValue = _configuration["YourSecretName"];
+            return Ok(new { Secret = secretValue });
+        }
+    }
+}
---
+++ AzureVaultSampleApi/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+namespace AzureVaultSampleApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureAppConfiguration((ctx, config) =>
+                {
+                    var root = config.Build();
+                    config.AddAzureKeyVault(
+                        $"https://{root["AzureKeyVault:Name"]}.vault.azure.net/",
+                        root["AzureKeyVault:ClientId"],
+                        root["AzureKeyVault:ClientSecret"]);
+                });
+    }
+}
